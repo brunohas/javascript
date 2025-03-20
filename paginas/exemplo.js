fc15dbb08d3923da899796f0dda9e8cebd4d6e8b
brunohas <brunolbp00@gmail.com>
2025-03-20 20:38:01 -0300

    update exemplo 2

---
+++ paginas/exemplo.js
@@ -0,0 +1,31 @@
+
+
+function entrou() {
+    let div = document.querySelector('div')
+    div.innerHTML = 'Entrou!'
+    div.style.backgroundColor = 'red'
+}
+
+function saiu() {
+    let div = document.querySelector('div')
+    div.innerHTML = 'Saiu!'
+    div.style.background = 'yellow'
+}
+
+function clicou() {
+    let div = document.querySelector('div')
+    div.innerHTML = 'Clicou!'
+    div.style.background = 'blue'
+}
+
+
+
+
+
+
+
+
+
+
+
+
